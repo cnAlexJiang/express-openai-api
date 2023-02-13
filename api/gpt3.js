8175a79e0fe42a173631c24da67f3792dbbd86ec
jiangyi <jiangyi@corp-ci.com>
2023-02-13 19:19:32 +0800

    init

---
+++ api/gpt3.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const router = express.Router()
+const { Configuration, OpenAIApi } = require('openai')
+const lodash = require('lodash')
+const configuration = new Configuration({
+  apiKey: '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',
+})
+const openai = new OpenAIApi(configuration)
+
+router.post('/ask', async (req, res) => {
+  console.log(req.body.content, typeof req.body, JSON.stringify(req.body))
+  try {
+    const { content } = req.body
+    if (!content) {
+      res.send('error')
+      res.json({
+        status: -1,
+        message: 'error',
+      })
+    } else {
+      openai
+        .createCompletion({
+          model: 'text-davinci-003',
+          prompt: content + ' \nA',
+          temperature: 0,
+          max_tokens: 100,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+          stop: ['\n'],
+        })
+        .then((response) => {
+  
+          const data = response.data
+          const temp = lodash.get(data, 'choices[0].text')
+          if (temp) {
+            res.json({
+              status: 200,
+              message: temp,
+            })
+
+          } else {
+            res.json({
+              status: -1,
+              message: 'error',
+            })
+          }
+        })
+    }
+
+  } catch (error) {
+    console.error(error)
+    return res.status(500).send('Server error')
+  }
+})
+
+module.exports = router
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require('cors')
+
+const app = express();
+const gpt3 = require("./api/gpt3");
+
+
+app.use(cors())
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.use("/api/gpt3", gpt3);
+
+const PORT = process.env.PORT || 8100;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
